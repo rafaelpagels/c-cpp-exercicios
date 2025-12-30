bca959b56011d6525f488394b7fe999a37d181c0
Rafael Pagels <raffx.320@gmail.com>
2025-12-29 22:28:30 -0300

    Add files via upload

---
+++ 02_intermediário/tabuada_usuario.cpp
@@ -0,0 +1,31 @@
+/*
+ * Programa em C/C++
+ * Exibe a tabuada de um número escolhido pelo usuário (1 a 10)
+ * Conteúdo: entrada de dados, for, validação
+ */
+
+#include <stdio.h>
+#include <locale.h>
+
+int numero, contador;
+
+int main (){
+    setlocale(LC_ALL, "PORTUGUESE");
+
+    printf("Digite um número para ver a tabuada: ");
+    scanf("%d", &numero);
+
+    	if (numero < 1) {
+        	printf("Número inválido.\n");
+        	return 0;
+    	}
+
+    printf("\nTabuada do %d:\n\n", numero);
+
+    	for (contador = 1; contador <= 10; contador++){
+        	printf("%d x %d = %d\n", numero, contador, numero * contador);
+    	}
+
+    return 0;
+}
+
